bdccbf9106ad7bc3b30ef3bdfd6a67b04c931406
Tomasz Milewski <tommilewski2003@gmail.com>
2025-04-15 10:10:05 +0200

    change mainfest.json

--- sw.js
+++ sw.js
@@ -64,15 +64,15 @@ self.addEventListener('fetch', (event) => {
     event.respondWith(
     caches.match(event.request).then((response) => {
         return response || fetch(event.request).then((fetchResponse) => {
-        return caches.open(cacheName).then((cache) => {
-        cache.put(event.request, fetchResponse.clone());
-        return fetchResponse;
-    });
+            return caches.open(cacheName).then((cache) => {
+            cache.put(event.request, fetchResponse.clone());
+            return fetchResponse;
+        });
     });
     }).catch(() => {
-        if (event.request.mode === 'navigate') {
-            return caches.match('/index.html');
-        }
-    })
+            if (event.request.mode === 'navigate') {
+                return caches.match('/index.html');
+            }
+        })
     );
 });
